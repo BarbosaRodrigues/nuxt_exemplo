960620425bed9a06414ceb2e3e5348660d9102b8
unknown <taisbarbosarodrigues2004@gmail.com>
2022-05-03 16:48:32 -0300

    Adicao projeto Nuxt.js

--- exemplo-front/pages/index.vue
+++ exemplo-front/pages/index.vue
@@ -1,9 +1,19 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h1>{{titulo}}</h1>
+    <input v-model="titulo" type="text" name="" id="">
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data: function(){
+    return{
+      titulo: "Página de exmplo"
+    }
+  }
 }
 </script>
+<style>
+</style>
